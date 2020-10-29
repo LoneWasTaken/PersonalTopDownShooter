cf6b95c3b9fca8ee72a32a30e247a6caaa4dbef3
LoneWasTaken <71338974+LoneWasTaken@users.noreply.github.com>
2020-10-29 10:52:18 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+
+    public Vector3 speed = new Vector3(50, 0, 50);
+
+    public Transform target;
+
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3 (pointToLook.x , transform.position.y, pointToLook.z));
+        }
+    }
+
+    public void Movement()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, 0, speed.z * inputZ);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
